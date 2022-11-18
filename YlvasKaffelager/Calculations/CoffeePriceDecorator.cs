5d8358621654597255369fc6e0e13eedb604f6d6
Patrik Beijar Odh <43520990+patrik033@users.noreply.github.com>
2022-11-18 15:36:18 +0100

    Added Comment

--- YlvasKaffelager/Calculations/CoffeePriceDecorator.cs
+++ YlvasKaffelager/Calculations/CoffeePriceDecorator.cs
@@ -6,6 +6,12 @@
         {
         }
 
+        /// <summary>
+        /// Returns the price for a coffee product with added taxes
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <param name="productPrice"></param>
+        /// <returns></returns>
         public override decimal CalculateTotalPrice(int amount, decimal productPrice)
         {
             //Taxes for coffee is 25% so the total price will be number of products bought times price and the taxes times the total price
